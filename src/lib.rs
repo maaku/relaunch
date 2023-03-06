a83354a13b7e4aaf85c2a84ccbf3405ba077b2fd
Mark Friedenbach <mark@friedenbach.org>
2023-03-06 11:54:57 -0800

    Remove cargo's example test.

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,4 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-
 // End of File
